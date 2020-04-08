a7339f096f8dfc796007cbea894ace88cb4e58b8
steven112163 <steven112163@gmail.com>
2020-04-08 20:26:05 +0800

    Initial files of hw2

---
+++ NP-HW2/server.py
@@ -0,0 +1,207 @@
+import sys
+import argparse
+import sqlite3
+from socketserver import ThreadingTCPServer, StreamRequestHandler
+
+
+class ThreadedServerHandler(StreamRequestHandler):
+    """
+    Class handling every request.
+    Every request from client will be handled by new thread.
+    """
+
+    def handle(self):
+        """
+        Function handling current connection.
+        It continuously listens to the client until client enters "exit".
+        :return: None
+        """
+        print('New connection.')
+        self.debug(f'Connection from {self.client_address[0]}({self.client_address[1]})')
+        self.conn = sqlite3.connect('server_0510002.db')
+        self.conn.row_factory = sqlite3.Row
+        self.wfile.write(bytes(
+            '********************************\n** Welcome to the BBS server. **\n********************************\n',
+            'utf-8'))
+        self.wfile.write(bytes('% ', 'utf-8'))
+        self.current_user = None
+        while True:
+            try:
+                command = str(self.rfile.readline(), 'utf-8').strip().split()
+                if command:
+                    if command[0] == 'exit':
+                        self.conn.close()
+                        self.debug(f'Exit from {self.client_address[0]}({self.client_address[1]})')
+                        return
+                    self.command_handler(command)
+                self.wfile.write(bytes('% ', 'utf-8'))
+            except Exception as e:
+                print(str(e))
+
+    def debug(self, log):
+        """
+        Print log for debugging
+        :param log: log for debugging
+        :return: None
+        """
+        global verbosity
+        if verbosity:
+            print(f'# {log}')
+            sys.stdout.flush()
+
+    def command_handler(self, command):
+        """
+        Function handling entered command
+        :param command: Command sent from client
+        :return: None
+        """
+        if command[0] == 'register':
+            self.register_handler(command)
+        elif command[0] == 'login':
+            self.login_handler(command)
+        elif command[0] == 'logout':
+            self.logout_handler(command)
+        elif command[0] == 'whoami':
+            self.whoami_handler(command)
+        else:
+            self.wfile.write(bytes('Invalid command\n', 'utf-8'))
+            self.debug(f'Invalid command from {self.client_address[0]}({self.client_address[1]})\n\t{command}')
+
+    def register_handler(self, command):
+        """
+        Function handling register command
+        :param command: Command sent from client
+        :return: None
+        """
+
+        # Check arguments
+        self.debug(f'Register from {self.client_address[0]}({self.client_address[1]})\n\t{command}')
+        if len(command) != 4:
+            self.wfile.write(bytes('Usage: register <username> <email> <password>\n', 'utf-8'))
+            self.debug(f'Incomplete register command from {self.client_address}({self.client_address})')
+            return
+
+        # Check whether username is used
+        cursor = self.conn.execute('SELECT username from USERS WHERE Username=:username', {"username": command[1]})
+        if cursor.fetchone() is not None:
+            self.wfile.write(bytes('Username is already used.\n', 'utf-8'))
+            self.debug(f'Username from {self.client_address[0]}({self.client_address[1]}) is used')
+            return
+
+        self.conn.execute('INSERT INTO USERS (Username, Email, Password) VALUES (:username, :email, :password)',
+                          {"username": command[1], "email": command[2], "password": command[3]})
+        self.conn.commit()
+        self.wfile.write(bytes('Register successfully.\n', 'utf-8'))
+        self.debug(f'Register successfully from {self.client_address[0]}({self.client_address[1]})')
+
+    def login_handler(self, command):
+        """
+        Function handling login command
+        :param command: Command sent from client
+        :return: None
+        """
+
+        # Check arguments
+        self.debug(f'Login from {self.client_address[0]}({self.client_address[1]})\n\t{command}')
+        if len(command) != 3:
+            self.wfile.write(bytes('Usage: login <username> <password>\n', 'utf-8'))
+            self.debug(f'Incomplete login command from {self.client_address[0]}({self.client_address[1]})')
+            return
+
+        # Check if user is already logged in
+        if self.current_user:
+            self.wfile.write(bytes(f'Please logout first.\n', 'utf-8'))
+            self.debug(f'User from {self.client_address[0]}({self.client_address[1]}) is already logged in')
+            return
+
+        cursor = self.conn.execute('SELECT Username, Password FROM USERS WHERE Username=:username',
+                                   {"username": command[1]})
+        row = cursor.fetchone()
+        if row is None:
+            self.wfile.write(bytes('Login failed.\n', 'utf-8'))
+            self.debug(f"Username entered from {self.client_address[0]}({self.client_address[1]}) isn't in DB")
+            return
+        if command[2] != row['Password']:
+            self.wfile.write(bytes('Login failed.\n', 'utf-8'))
+            self.debug(f'Password entered from {self.client_address[0]}({self.client_address[1]}) is wrong')
+            return
+
+        self.current_user = row['Username']
+        self.wfile.write(bytes(f'Welcome, {self.current_user}.\n', 'utf-8'))
+        self.debug(f'Log in successfully from {self.client_address[0]}({self.client_address[1]})')
+
+    def logout_handler(self, command):
+        """
+        Function handling logout command
+        :param command: Command sent from client
+        :return: None
+        """
+        self.debug(f'Logout from {self.client_address[0]}({self.client_address[1]})\n\t{command}')
+        if self.current_user:
+            self.wfile.write(bytes(f'Bye, {self.current_user}.\n', 'utf-8'))
+            self.current_user = None
+            self.debug(f'User from {self.client_address[0]}({self.client_address[1]}) log out')
+        else:
+            self.wfile.write(bytes('Please login first.\n', 'utf-8'))
+            self.debug(f'User from {self.client_address[0]}({self.client_address[1]}) is already logged out')
+
+    def whoami_handler(self, command):
+        """
+        Function handling whoami command
+        :param command: Command sent from client
+        :return: None
+        """
+        self.debug(f'Whoami from {self.client_address[0]}({self.client_address[1]})\n\t{command}')
+        if self.current_user:
+            self.wfile.write(bytes(f'{self.current_user}.\n', 'utf-8'))
+            self.debug(f'Current user check from {self.client_address[0]}({self.client_address[1]})')
+        else:
+            self.wfile.write(bytes('Please login first.\n', 'utf-8'))
+            self.debug(f'User from {self.client_address[0]}({self.client_address[1]}) is already logged out')
+
+
+def parse_arguments():
+    """
+    Generate an argument parser
+    :return: argparse.Namespace an object containing all required arguments
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('port', help='port number', nargs='?', type=int)
+    parser.add_argument('-v', '--verbosity', help='verbosity level', action='store_true')
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    """
+    Main running server
+    Setup with python3 ./server.py <port>
+    :return: None
+    """
+
+    # Check arguments, host and port
+    host = 'localhost'
+    args = parse_arguments()
+    port = args.port
+
+    verbosity = args.verbosity
+
+    # Create database and table
+    conn = sqlite3.connect('server_0510002.db')
+    cursor = conn.execute('SELECT name FROM sqlite_master WHERE type="table" AND name="USERS";')
+    if cursor.fetchone() is None:
+        conn.execute('''CREATE TABLE USERS (
+                            UID      INTEGER PRIMARY KEY AUTOINCREMENT,
+                            Username TEXT UNIQUE NOT NULL,
+                            Email    TEXT NOT NULL,
+                            Password TEXT NOT NULL
+                        );''')
+    conn.close()
+
+    ThreadingTCPServer.allow_reuse_address = True
+    ThreadingTCPServer.daemon_threads = True
+    with ThreadingTCPServer((host, port), ThreadedServerHandler) as server:
+        try:
+            server.serve_forever()
+        except KeyboardInterrupt:
+            print('\n')
+            server.shutdown()
